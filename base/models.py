5bfc4ad757f4ffe486d29daaa63aef9318bab5c1
kumanik5661 <nikhilkumar654231@gmail.com>
2020-05-16 21:41:04 +0530

    Model Changes Views added

--- base/models.py
+++ base/models.py
@@ -1,18 +1,20 @@
 from mongoengine import *
 
 
-class Event(DynamicEmbeddedDocument):
-    single_input = DictField()
-
-
 class Student(DynamicEmbeddedDocument):
+    name = StringField(max_length=256)
     single_input = DictField()
 
 
-class StudentList(DynamicDocument):
-    event = EmbeddedDocumentField(Event)
+class StudentList(DynamicEmbeddedDocument):
     type = StringField(max_length=256)
     list = ListField(EmbeddedDocumentField(Student))
+
+
+class Event(DynamicDocument):
+    name = StringField(max_length=256)
+    single_input = DictField()
+    student_lists = ListField(StudentList)
 
 
 class File(Document):
--- base/views.py
+++ base/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
 
-# Create your views here.
+
+def listEvents(request):
+    list_events = Event.objects.order_by['name']
+    return render(request, "", list_events )
+
+
+def listStudents(request, event_id):
+    event = get_object_or_404(Event, id=event_id)
+    student_lists = event.student_lists
+    return render(request, '', student_lists )
