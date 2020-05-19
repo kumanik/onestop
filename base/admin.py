33e9118d72cf9e3c3837234289644054e79a6a38
isha27255 <56606434+isha27255@users.noreply.github.com>
2020-05-19 12:56:14 +0530

    Update admin.py

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,1 @@
-from django.contrib import admin
-from import_export.admin import ImportExportModelAdmin
-from .models import Student
 
-@admin.register(Student)
-class StudentAdmin(ImportExportModelAdmin):
-  list_display = ('name','email')
