cc59c9c56f7f7b64f56a426f80fc788552d88669
kumanik5661 <nikhilkumar654231@gmail.com>
2020-06-02 23:24:06 +0530

    Solved mongoengine:ConnectionError

--- base/models.py
+++ base/models.py
@@ -4,13 +4,13 @@ from decouple import config
 
 class Student(DynamicDocument):
     name = StringField(max_length=256)
-    meta = {'db_alias': 'mongo'}
+    meta = {'db_alias': 'default'}
 
 
 class StudentList(DynamicDocument):
     type = StringField(max_length=256)
     list = ListField(ReferenceField(Student, reverse_delete_rule=PULL))
-    meta = {'db_alias': 'mongo'}
+    meta = {'db_alias': 'default'}
 
     def delete(self, *args, **kwargs):
         for student in self.list:
@@ -21,10 +21,10 @@ class StudentList(DynamicDocument):
 class Event(DynamicDocument):
     name = StringField(max_length=256)
     student_lists = ListField(ReferenceField(StudentList, reverse_delete_rule=PULL))
-    meta = {'db_alias': 'mongo'}
+    meta = {'db_alias': 'default'}
 
 
 class File(Document):
     file = FileField()
     input = StringField(max_length=256)
-    meta = {'db_alias': 'mongo'}
+    meta = {'db_alias': 'default'}
