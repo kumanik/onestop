cf119ff72641c98803273be31c0426b289d79892
kumanik5661 <nikhilkumar654231@gmail.com>
2020-06-14 04:50:58 +0530

    Added classes for testing

--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,43 @@
+import mongoengine
 from django.test import TestCase
+from django.conf import settings
 
-# Create your tests here.
+
+class MongoTestCase(TestCase):
+    """Instead of using django 'TestCase' class. Use this in tests.
+    It will overwrite the 'setUp' and 'tearDown' functions of django 'TestCase'.
+
+    For example->
+    In tests.py :
+
+    from models import App
+
+    class AppCreationTest(MongoTestCase):
+      def test(self):
+        app = App(name="first_app")
+        app.save()
+        assert App.objects.first().name == app.name
+        
+    In terminal :
+
+    python manage.py test
+
+    Note: Dont forget to specify test db details in settings.
+    """
+    def setUp(self):
+        mongoengine.connection.disconnect()
+        mongoengine.connect(
+            host=settings.MONGO['host'],
+            port=settings.MONGO['port'],
+            db=settings.MONGO['db'],
+            username=settings.MONGO['username'],
+            password=settings.MONGO['password']
+            )
+        super().setUpClass()
+
+    def tearDown(self):
+        from mongoengine.connection import get_connection, disconnect
+        connection = get_connection()
+        connection.drop_database(settings.MONGO['db'])
+        disconnect()
+        super().tearDownClass()
