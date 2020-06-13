22f79f8eb0d0a846bb43cc843621c40782ae4ec9
kumanik5661 <nikhilkumar654231@gmail.com>
2020-06-14 04:42:24 +0530

    Template changes more features available from the event details page

--- base/urls.py
+++ base/urls.py
@@ -4,11 +4,19 @@ from base import views
 
 urlpatterns = [
     path("", listEvents, name="index"),
+
     path("addEvent", create_event, name="addEvent"),
+
     path("event/<event_id>", viewEvent, name="viewEvent"),
+
     path("event/studentlist/<list_id>", viewStudentList, name="viewStudents"),
+
     path("event/delete/<event_id>", deleteEvent, name="deleteEvent"),
+
     path("event/update/<event_id>", updateEvent, name="updateEvent"),
+
     path("event/student_list/delete/<list_id>", deleteStudentList, name="deleteStudentList"),
+
     path("event/addstudentlist/upload/<event_id>", upload_student_list, name="upload_studentlist"),
+
 ]
--- base/views.py
+++ base/views.py
@@ -24,58 +24,9 @@ def viewEvent(request, event_id):
         event_dict.pop('name')
     except:
         pass
-    return render(request, 'base/eventDetails.html', {'event': event, 'event_dict': event_dict})
-
-
-@login_required
-def viewStudentList(request, list_id):
-    student_list = StudentList.objects.get(id=list_id)
-    event = Event.objects.get(student_lists__contains=student_list.id)
-    return render(request, "base/studentList.html", {'student_list': student_list, 'event': event})
-
-
-@login_required
-def deleteStudentList(request, list_id):
-    list1 = StudentList.objects.get(id=list_id)
-    event = Event.objects.get(student_lists__contains=list1.id)
-    list1.delete()
-    return redirect('viewEvent', event.id)
-
-
-def handle_uploaded_file(f):
-    with open('base/upload/' + f.name, 'wb+') as destination:
-        for chunk in f.chunks():
-            destination.write(chunk)
-    if f.name.endswith('.xlsx'):
-        data_xls = pd.read_excel('base/upload/' + f.name)
-        data_xls.to_csv('base/upload/' + f.name, encoding='utf-8', index=False)
-    df = pd.read_csv('base/upload/' + f.name)
-    df.drop_duplicates(inplace=True)
-    df.to_csv('base/upload/' + f.name, index=False)
-    
-    
-@login_required
-def upload_student_list(request, event_id):
-    if request.method == 'POST':
-        handle_uploaded_file(request.FILES['file'])
-        data = request.FILES['file']
-        type1 = request.POST.get('input')
-        list1 = StudentList(type=type1)
-        event = Event.objects.get(id=event_id)
-        with open('base/upload/' + data.name, 'r') as csv_file:
-            datas = csv.DictReader(csv_file)
-            for row in datas:
-                stu = Student(**row)
-                stu.save()
-                list1.list.append(stu)
-        os.remove('base/upload/' + data.name)
-        list1.save()
-        event.student_lists.append(list1)
-        event.save()
-        return redirect('viewEvent', event_id)
-    else:
-        form = FileForm()
-    return render(request, "base/upload_file.html", {'form': form})
+    count = len(event.student_lists)
+    print(count)
+    return render(request, 'base/eventDetails.html', {'event': event, 'event_dict': event_dict, 'count': count})
 
 
 @login_required
@@ -111,3 +62,54 @@ def updateEvent(request, event_id):
         event.save()
         return redirect('viewEvent', event.id)
     return render(request, 'base/update_event.html', {'event': event, 'event_dict': event_dict})
+
+
+@login_required
+def viewStudentList(request, list_id):
+    student_list = StudentList.objects.get(id=list_id)
+    event = Event.objects.get(student_lists__contains=student_list.id)
+    return render(request, "base/studentList.html", {'student_list': student_list, 'event': event})
+
+
+@login_required
+def deleteStudentList(request, list_id):
+    list1 = StudentList.objects.get(id=list_id)
+    event = Event.objects.get(student_lists__contains=list1.id)
+    list1.delete()
+    return redirect('viewEvent', event.id)
+
+
+def handle_uploaded_file(f):
+    with open('base/upload/' + f.name, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+    if f.name.endswith('.xlsx'):
+        data_xls = pd.read_excel('base/upload/' + f.name)
+        data_xls.to_csv('base/upload/' + f.name, encoding='utf-8', index=False)
+    df = pd.read_csv('base/upload/' + f.name)
+    df.drop_duplicates(inplace=True)
+    df.to_csv('base/upload/' + f.name, index=False)
+
+
+@login_required
+def upload_student_list(request, event_id):
+    if request.method == 'POST':
+        handle_uploaded_file(request.FILES['file'])
+        data = request.FILES['file']
+        type1 = request.POST.get('input')
+        list1 = StudentList(type=type1)
+        event = Event.objects.get(id=event_id)
+        with open('base/upload/' + data.name, 'r') as csv_file:
+            datas = csv.DictReader(csv_file)
+            for row in datas:
+                stu = Student(**row)
+                stu.save()
+                list1.list.append(stu)
+        os.remove('base/upload/' + data.name)
+        list1.save()
+        event.student_lists.append(list1)
+        event.save()
+        return redirect('viewEvent', event_id)
+    else:
+        form = FileForm()
+    return render(request, "base/upload_file.html", {'form': form})
