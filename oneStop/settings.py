a9557df6b83bc1e2977b8d6149f9203f427816e3
kumanik5661 <nikhilkumar654231@gmail.com>
2020-06-18 17:36:19 +0000

    Debug = True

--- oneStop/settings.py
+++ oneStop/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "one-1-stop.herokuapp.com",
